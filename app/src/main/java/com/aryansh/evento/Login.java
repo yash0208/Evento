276c24567e49d2459939c087b5039709c25a4455
Yash <yashrajmehta3027@gmail.com>
2023-05-02 21:04:36 +0530

    Initial commit

---
+++ app/src/main/java/com/aryansh/evento/Login.java
@@ -0,0 +1,19 @@
+package com.aryansh.evento;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view) {
+
+    }
+}
